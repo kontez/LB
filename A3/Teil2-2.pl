09179f46823a5645bde855eff245c5e11baf3f28
Marvin Contessi <kontez@users.noreply.github.com>
2016-11-09 14:10:52 +0100

    I'll cut you

--- A3/Teil2-2.pl
+++ A3/Teil2-2.pl
@@ -1,16 +1,16 @@
 % my_unify
 
 % Term1 und Term2 sind Variablen
-my_unify(Term1, Term2) :- var(Term1), var(Term2), !, Term1 = Term2.
+my_unify(Term1, Term2) :- var(Term1), var(Term2), Term1 = Term2.
 
 % Term1 ist eine Variable und Term2 nicht
-my_unify(Term1, Term2) :- var(Term1), nonvar(Term2), !, occurs_check(Term1, Term2), Term1 = Term2.
+my_unify(Term1, Term2) :- var(Term1), nonvar(Term2), occurs_check(Term1, Term2), Term1 = Term2.
 
 % Term1 ist keine Variable aber Term1 ja
-my_unify(Term1, Term2) :- nonvar(Term1), var(Term2), !, occurs_check(Term2, Term1), Term2 = Term1.
+my_unify(Term1, Term2) :- nonvar(Term1), var(Term2), occurs_check(Term2, Term1), Term2 = Term1.
 
 % Keine Variable
-my_unify(Term1, Term2) :- nonvar(Term1), nonvar(Term2), !,
+my_unify(Term1, Term2) :- nonvar(Term1), nonvar(Term2),
                           Term1 =.. [TName1|TListe1],
                           Term2 =.. [TName2|TListe2],
                           TName1 == TName2, %Die Namen der Praedikate muessen gleich sein
@@ -33,14 +33,14 @@ my_unify_list([Kopf1|Rest1], [Kopf2|Rest2]) :- my_unify(Kopf1, Kopf2), my_unify_
 % occurs_check
 
 % Falls t eine Variable, und t ungleich X
-occurs_check(X, Term) :- var(Term), !, Term \== X, !.
+occurs_check(X, Term) :- var(Term), Term \== X.
 
 % Falls Term keine Variable.
 % Wir suchen in den Argumenten von Term, nach X
 occurs_check(X, Term) :- nonvar(Term), Term =.. [_Kopf|Rest], occurs_check_list(X, Rest).
 
 % X ist nicht in den Argumenten.
-occurs_check_list(_, []) :- !.
+occurs_check_list(_, []).
 
 % rekursives suchen nach X.
 occurs_check_list(X, [Kopf|Rest]) :- occurs_check(X, Kopf), occurs_check_list(X, Rest).
